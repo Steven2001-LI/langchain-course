31a8e79e381e6d260f51177c2fd6101dc2f47928
Steven Li <Steven20010101@outlook.com>
2026-07-08 11:41:59 +1000

    practice: personalize hello world print message

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 def main():
-    print("Hello from langchain-course!")
+    print("Hello from langchain-course! -- Steven's practice")
     information = """
     Elon Reeve Musk FRS (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman, known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
 
